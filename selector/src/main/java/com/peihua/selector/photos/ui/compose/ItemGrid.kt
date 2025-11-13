768e6d6ef11ce0470a148b926e33a75057eb5c9a
peihua <379046428@qq.com>
2025-11-13 23:37:27 +0800

    1、增加功能，修复bug

--- selector/src/main/java/com/peihua/selector/photos/ui/compose/ItemGrid.kt
+++ selector/src/main/java/com/peihua/selector/photos/ui/compose/ItemGrid.kt
@@ -1,14 +1,24 @@
 package com.peihua.selector.photos.ui.compose
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.VideoCall
+import androidx.compose.material.icons.filled.Videocam
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
+import com.fz.common.utils.dLog
 import com.peihua.photopicker.R
 import com.peihua.selector.data.model.Item
 
@@ -39,7 +49,11 @@ fun ItemGrid(item: Item, isSelected: Boolean, isSelectMultiple: Boolean, onItemC
 @Composable
 fun ItemPhotoGrid(item: Item) {
     Box {
-        AsyncImage(model = item.contentUri, contentDescription = null)
+        AsyncImage(
+            modifier = Modifier.aspectRatio(1f),
+            contentScale = ContentScale.Crop,
+            model = item.contentUri, contentDescription = null
+        )
     }
 }
 
@@ -47,6 +61,15 @@ fun ItemPhotoGrid(item: Item) {
 fun ItemVideoGrid(item: Item) {
     Box {
         AsyncImage(model = item.videoThumbnail, contentDescription = null)
+        dLog { "item>>>>$item" }
+        Row(modifier = Modifier.align(Alignment.BottomStart)) {
+            Icon(
+                imageVector = Icons.Default.Videocam,
+                tint = Color.White,
+                contentDescription = null
+            )
+            Text(text = item.durationText, color = Color.White)
+        }
     }
 }
 
